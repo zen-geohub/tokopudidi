f87de5721f23921fd93e0aec2e461e9670d6a9a3
zen-geohub <abidinzaenal556@gmail.com>
2025-03-06 16:04:30 +0700

    feat: cart

--- routers/user.js
+++ routers/user.js
@@ -9,10 +9,9 @@ router.get('/products/:id', Products.detailProductByUser)
 
 // cart
 router.get('/cart', Cart.readCart)
-router.post('/cart/add/:id', Cart.addCart)
+router.get('/cart/increment/:id', Cart.incrementProduct)
+router.get('/cart/decrement/:id', Cart.decrementProduct)
+router.get('/cart/add/:id', Cart.addCart)
 router.get('/cart/delete/:id', Cart.deleteCart)
-
-//history
-router.get('/orders', Cart.readHistory)
 
 module.exports = router
